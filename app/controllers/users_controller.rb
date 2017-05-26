da7068a7f241150b5d23ef03cca1751c9daffd89
darthpasta <liowj.nick@gmail.com>
2017-05-27 02:21:16 +0800

    added create method to sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create 
+		@user = User.create(user_params)
+		if @user.save
+			# flash = [success: "Successfully signed up "]
+			redirect_to root_path
+		else
+			# flash = [error: "Sign up failed"]
+			redirect_to users_new_path
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :email, :password)
+		end
+
 end
