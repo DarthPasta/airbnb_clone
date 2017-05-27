ae465385f9841c02c00cabad4abb6c5a463815b0
darthpasta <liowj.nick@gmail.com>
2017-05-27 16:49:49 +0800

    Users can now log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 	has_many :reservations 
 	has_many :listings, through: :reservations
+
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 	root 'users#index'
 	get '/users/new' => 'users#new'
 	resources :users
+
+	get '/sessions/new' => 'sessions#new'
+	post '/sessions/create' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
