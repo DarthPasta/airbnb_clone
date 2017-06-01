556a9cb1894f5850bffac596430a18238cfb3b5f
darthpasta <liowj.nick@gmail.com>
2017-06-01 13:47:19 +0800

    Fix bug in user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530174415) do
+ActiveRecord::Schema.define(version: 20170531124704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170530174415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "reservations", force: :cascade do |t|
