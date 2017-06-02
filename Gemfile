2a75deedb4bb4ed95a0442e2dd32eca7d44b94bc
darthpasta <liowj.nick@gmail.com>
2017-06-02 22:15:07 +0800

    added calendar

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 
 gem 'figaro'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
+//= require jquery_ujs 
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require_tree .
 
